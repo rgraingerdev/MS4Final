0da4fdf3169ce638aceeddeb39163eb648ccafe4
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2024-02-14 15:43:27 +0000

    Basket template in Place ready to add asset DB

--- megashop/megashop/urls.py
+++ megashop/megashop/urls.py
@@ -18,10 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import homepage
-
+from basket.views import basket
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', homepage, name='homepage'),
+    path('basket', basket, name='basket'),
 ]
--- megashop/megashop/views.py
+++ megashop/megashop/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, './base.html')
+    return render(request, './megashop/base.html')
