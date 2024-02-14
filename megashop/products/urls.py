f6005516ba43ece945d63322361cd01871cd59d7
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2024-02-14 15:52:04 +0000

    Created a producs page template in prep for models

--- megashop/products/urls.py
+++ megashop/products/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for megashop project.
+URL configuration for products project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,16 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from .views import homepage
-from basket.views import basket
-from products.views import products
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', homepage, name='homepage'),
-    path('basket', basket, name='basket'),
-    path('products', products, name='products'),
-
+    path("products/", name='products'),
 ]
