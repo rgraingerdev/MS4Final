0146e0e6c0f88aeba5c332da381902b2425da217
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2024-02-22 09:57:18 +0000

    Updated account View form and view

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,30 +5,21 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
+        fields = [
+            'default_phone_number',
+            'default_street_address1',
+            'default_street_address2',
+            'default_town_or_city',
+            'default_county',
+            'default_postcode',
+            'default_country',
+        ]
+        labels = {
             'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
             'default_street_address1': 'Street Address 1',
             'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County',
+            'default_postcode': 'Post Code',
+            'default_country': 'Country',
         }
-
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False
