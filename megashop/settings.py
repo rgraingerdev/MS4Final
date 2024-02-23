d66fa49fd81ca44f7b7a0cdde49057da698b6a5b
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2024-02-23 12:49:06 +0000

    Change for heroku deploy

--- megashop/settings.py
+++ megashop/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    "allauth.account.middleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     
 ]
@@ -160,7 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles.')
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
