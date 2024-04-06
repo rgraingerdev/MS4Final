12c937d843539cf21e43d9a5102af21275e7f35a
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2024-04-06 09:19:39 +0000

    Changes in line with validator

--- megashop/settings.py
+++ megashop/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-=e138*vz#30p5lzhn&-d_(b6nr_$v^49=trsbh9a(y6%49t+)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['megashop-d3bc08f15f0a.herokuapp.com', 'localhost/']
+ALLOWED_HOSTS = ['megashop-d3bc08f15f0a.herokuapp.com', 'localhost']
 
 
 # Application definition
